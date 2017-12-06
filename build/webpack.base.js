69527730d9c0e2c45153ec0934d97f09efaf9819
sfyr111 <sfyr111@163.com>
2017-12-06 15:26:40 +0800

    完成工程架构

---
+++ build/webpack.base.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/public/' // 要加斜杠，不然热更新会出错
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre', // 编译钱eslint
+        test: /.(js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: [
+          path.resolve(__dirname, '../node_modules')
+        ]
+      },
+      {
+        test: /.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: [
+          path.join(__dirname, '../node_modules')
+        ]
+      }
+    ]
+  },
+}
